5d0e9a0f041aa7cff5f54f34d29d8b1bb986f534
aaurizon <750048+aaurizon@users.noreply.github.com>
2025-04-19 19:51:26 +0200

    Add PromiseInterface definition
    Introduced a new PromiseInterface in the A\Async namespace. This interface defines methods for handling promise resolution (`then`), rejection (`catch`), and finalization (`finally`).

---
+++ src/A/Async/PromiseInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace A\Async;
+
+interface PromiseInterface
+{
+    public function then(callable $on_fulfilled): \A\Async\PromiseInterface;
+
+    public function catch(callable $on_rejected, string $exception_classname = \Throwable::class): \A\Async\PromiseInterface;
+
+    public function finally(callable $on_finally): \A\Async\PromiseInterface;
+}
