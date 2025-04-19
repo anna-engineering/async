c4e9b1ad6efd289b2c151a0c57c99816cfeb8748
aaurizon <750048+aaurizon@users.noreply.github.com>
2025-04-19 20:04:18 +0200

    Add utility functions 'asleep' and 'await'
    Introduced two new functions in `functions.php`: `asleep`, which provides a sleep mechanism compatible with fibers, and a placeholder `await` function. These utilities aim to support asynchronous operations.

---
+++ src/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+if (!function_exists('asleep'))
+{
+    function asleep(float $seconds) : void
+    {
+        if ($fiber = \Fiber::getCurrent())
+        {
+            for ($until = hrtime(true) + ceil($seconds * 1000 * 1000 * 1000) ; hrtime(true) < $until ; )
+            {
+                $fiber->suspend();
+            }
+        }
+        else
+        {
+            $microseconds = ceil($seconds * 1000 * 1000);
+
+            usleep($microseconds);
+        }
+    }
+}
+
+if (!function_exists('await'))
+{
+    function await()
+    {
+    }
+}
