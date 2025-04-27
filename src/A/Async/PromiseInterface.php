2c64c308dce455f341d7b34723093a23c7af1443
aaurizon <750048+aaurizon@users.noreply.github.com>
2025-04-27 15:56:31 +0200

    Add `wait` method to PromiseInterface
    Introduced a new `wait` method to the PromiseInterface, returning a `mixed` type. Also added consistent spacing before return type declarations for better readability.

--- src/A/Async/PromiseInterface.php
+++ src/A/Async/PromiseInterface.php
@@ -4,9 +4,11 @@ namespace A\Async;
 
 interface PromiseInterface
 {
-    public function then(callable $on_fulfilled): \A\Async\PromiseInterface;
+    public function then(callable $on_fulfilled) : \A\Async\PromiseInterface;
 
-    public function catch(callable $on_rejected, string $exception_classname = \Throwable::class): \A\Async\PromiseInterface;
+    public function catch(callable $on_rejected, string $exception_classname = \Throwable::class) : \A\Async\PromiseInterface;
 
-    public function finally(callable $on_finally): \A\Async\PromiseInterface;
+    public function finally(callable $on_finally) : \A\Async\PromiseInterface;
+
+    public function wait() : mixed;
 }
